43a3d4ec2069709658b9d6a715148b94fcab15c7
Adrian Thummerer <adrian.thummerer@med.uni-muenchen.de>
2026-04-27 22:15:32 +0200

    option to run multiple stages in sequence

--- run_preprocessing.py
+++ run_preprocessing.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run preprocessing for patients.")
     parser.add_argument('-config_file', '-c', type=str, help='Path to the configuration file.')
     parser.add_argument('-device', '-d', type=str, default='cpu', help='Device to use for processing, e.g. cpu for CPU, cuda:0 for GPU 0,')
-    parser.add_argument('-stage', '-s', type=int, default=1, help='Preprocessing stage to run (1, 2, or 3).')
+    parser.add_argument('-stage', '-s', type=int, nargs='+', default=None, help='Stages to run, e.g. -s 1 2 3')
     parser.add_argument('--overview', action='store_true', help='Generate an overview image without running the preprocessing pipeline.')
     parser.add_argument('--skip_recon', action='store_true', help='Skip rtk CBCT reconstruction.')
     parser.add_argument('--correct_contrast_media', action='store_true', help='Enable contrast media correction in stage 3 (requires PyTorch).')
@@ -22,28 +22,32 @@ if __name__ == "__main__":
 
     for patient_id, config in configs.items():
         try:
-            logger.info(f"--- Processing patient {patient_id} ---")
+            logger.info(f"--- Processing patient {patient_id} --- stage(s): {args.stage} ---")
             if args.correct_contrast_media:
                 config.settings.correct_contrast_media = True
             processor = PreProcessor(patient_id, config, device=args.device, skip_recon=args.skip_recon)
             # delete files for stage 2 or 3
-            if args.cleanup:
-                if args.stage == 2:
-                    processor.cleanup_s2()
-                elif args.stage == 3:
-                    processor.cleanup_s3()
-            elif args.stage == 1:
-                if args.overview:
-                    processor.generate_overview_image()
-                else:
-                    processor.run_stage1()
-            elif args.stage == 2:
-                if args.overview:
-                    processor.generate_overview_deformed()
-                else:
-                    processor.run_stage2()
-            elif args.stage == 3:
-                processor.run_stage3()
+            for stage in args.stage:
+                if args.cleanup:
+                    if stage == 2:
+                        processor.cleanup_s2()
+                    elif stage == 3:
+                        processor.cleanup_s3()
+
+                elif stage == 1:
+                    if args.overview:
+                        processor.generate_overview_image()
+                    else:
+                        processor.run_stage1()
+
+                elif stage == 2:
+                    if args.overview:
+                        processor.generate_overview_deformed()
+                    else:
+                        processor.run_stage2()
+
+                elif stage == 3:
+                    processor.run_stage3()
             logger.info(f"--- Successfully finished patient {patient_id} ---")
 
         except Exception as e:
